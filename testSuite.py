2affcf2656dd18f41ac37a300c6b580e363bac8c
Taryn Sanders <tosander@usc.edu>
2023-10-20 15:23:40 -0700

    Attempt 323

--- testSuite.py
+++ testSuite.py
@@ -8,7 +8,7 @@ GPIO.setmode(GPIO.BOARD)
 led_pin = 11
 GPIO.setup(led_pin, GPIO.OUT)
 
-SPI_PORT = 1
+SPI_PORT = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
